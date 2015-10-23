d9e852b337415d27e868b79ce0caf51d64b18dd7
56KBs <matt@infusioncomputing.com>
2015-10-23 10:15:02 +0100

    Implementation and test additions
    Implement Euler Totient and Greatest Common Factor.
    
    Add tests for both, ensure 100% test coverage

--- js/MathFactory.js
+++ js/MathFactory.js
@@ -14,5 +14,67 @@ angular.module('websiteApp.MathFactory').factory('MathFactory', function()
 		return Math.floor(numerator / denominator);
 	}
 
+	MathFactory.GreatestCommonFactor = function(a, b)
+	{
+		if (a == b)
+		{
+			return a;
+		}
+		else if (a == 0)
+		{
+			return b;
+		}
+		else if (b == 0)
+		{
+			return a;
+		}
+
+		if (MathFactory.IsEven(a))
+		{
+			if (MathFactory.IsEven(b))
+			{
+				return MathFactory.GreatestCommonFactor(MathFactory.IntDivide(a, 2), MathFactory.IntDivide(b, 2)) * 2;
+			}
+			else
+			{
+				return MathFactory.GreatestCommonFactor(MathFactory.IntDivide(a, 2), b);
+			}
+		}
+		else
+		{
+			if (MathFactory.IsEven(b))
+			{
+				return MathFactory.GreatestCommonFactor(a, MathFactory.IntDivide(b, 2));
+			}
+			else
+			{
+				if (a >= b)
+				{
+					return MathFactory.GreatestCommonFactor(MathFactory.IntDivide((a-b), 2), b);
+				}
+				else
+				{
+					return MathFactory.GreatestCommonFactor(a, MathFactory.IntDivide((b-a), 2));
+				}
+			}
+		}
+	}
+
+	MathFactory.EulerTotientRSA = function(n, p, q)
+	{
+		// This is a simplified version, specific to RSA usage
+		return (n - (p + q - 1));
+	}
+
+	MathFactory.IsEven = function(number)
+	{
+		return number % 2 === 0;
+	}
+
+	MathFactory.IsOdd = function(number)
+	{
+		return !MathFactory.IsEven(number);
+	}
+
 	return MathFactory;
 });
--- test/MathSpec.js
+++ test/MathSpec.js
@@ -39,12 +39,49 @@ describe("MathFactory", function() {
 		});
 	});
 
+	describe("IsEven", function() {
+		it("correctly identifies even numbers", function() {
+			expect(MathFactory.IsEven(2)).toBe(true);
+			expect(MathFactory.IsEven(10368548)).toBe(true);
+			expect(MathFactory.IsEven(23453212)).toBe(true);
+		});
+
+		it("correctly identifies odd numbers", function() {
+			expect(MathFactory.IsEven(3)).toBe(false);
+			expect(MathFactory.IsEven(46541)).toBe(false);
+			expect(MathFactory.IsEven(13245)).toBe(false);
+		});
+	});
+
+	describe("IsOdd", function() {
+		it("correctly identifies even numbers", function() {
+			expect(MathFactory.IsOdd(2)).toBe(false);
+			expect(MathFactory.IsOdd(10368548)).toBe(false);
+			expect(MathFactory.IsOdd(23453212)).toBe(false);
+		});
+
+		it("correctly identifies odd numbers", function() {
+			expect(MathFactory.IsOdd(3)).toBe(true);
+			expect(MathFactory.IsOdd(46541)).toBe(true);
+			expect(MathFactory.IsOdd(13245)).toBe(true);
+		});
+	});
+
+
 	describe("GreatestCommonFactor", function() {
-		it ("correctly identifies greatest common factors", function() {
+		it("correctly identifies greatest common factors", function() {
 			expect(MathFactory.GreatestCommonFactor(10, 2)).toEqual(2);
 			expect(MathFactory.GreatestCommonFactor(24, 108)).toEqual(12);
 			expect(MathFactory.GreatestCommonFactor(9, 12)).toEqual(3);
 			expect(MathFactory.GreatestCommonFactor(100, 17)).toEqual(1);
+			expect(MathFactory.GreatestCommonFactor(0, 100)).toEqual(100);
+			expect(MathFactory.GreatestCommonFactor(100, 0)).toEqual(100);
+		});
+	});
+
+	describe("EulerTotientRSA", function() {
+		it("correctly calculates the Euler Totient for RSA primes", function() {
+			expect(MathFactory.EulerTotientRSA(91, 7, 13)).toEqual(72);
 		});
 	});
 });
